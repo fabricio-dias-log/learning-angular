8f13aaaceae1489ab54173cb8cf71355d3681a64
Mitsunashi <fabd270909@gmail.com>
2022-03-02 17:13:28 -0300

    Config de Hack e Ciclo de VIda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-title title="hello world"></app-title>
+    <router-outlet></router-outlet>
+    `, // verifica se tem rotas definidas no angular e carrega o que tiver na pagina
 })
-export class AppComponent {
-  title = 'first-app';
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { //adicionar eventos ou funcionalidades ao iniciar a aplicação
+    setTimeout(() => {
+      console.log("teste");
+
+    }, 5000);
+
+  }
+
+
 }
---
+++ src/app/title/title.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent implements OnInit,OnChanges {
+
+  @Input() public title:string = "Bem-Vindo!";
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngOnChanges(): void {
+    console.log("Alterado com sucesso!");
+
+  }
+}
