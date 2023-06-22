c5f58d64dd5dac700f6bb34a4cfcd4667004ffa2
Fabricio Dias <fabricio270909@gmail.com>
2023-06-22 14:21:11 -0300

    ngIf e ngFor

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasEstruturaisComponent implements OnInit {
 
+  public condition: boolean = true;
+  public conditionClick: boolean = true;
+
+  public list: Array<{nome: string, idade: number}> = [
+    {nome: "Fabricio", idade: 22},
+    {nome: "César", idade: 35},
+    {nome: "Gabriel", idade: 45},
+    {nome: "Lucas", idade: 67},
+    {nome: "Rafael", idade: 11}
+  ];
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    //ngIf
+    setInterval(()=>{
+      if (this.condition) {
+        this.condition = false;
+      }else{
+        this.condition = true;
+      }
+    },2000);
+  }
+  //ngIf
+  public handleClick(): void{
+    if (this.conditionClick) {
+      this.conditionClick = false;
+    }else{
+      this.conditionClick = true;
+    }
+  }
+  public handleClickAddList(): void{
+    this.list.push({nome: "Otávio", idade: 33});
+  }
+  public handleClickEventList(event: number): void{
+    this.list.splice(event, 1);
+  }
 }
